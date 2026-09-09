a90c3622542f73cab294809b693feb1d499110e9
Umair Ali <syedumairzaidi10@gmail.com>
2026-09-09 19:46:53 +0500

    Update contact manager messages

--- project2(Phone contacts manager).py
+++ project2(Phone contacts manager).py
@@ -15,21 +15,21 @@ if option=="1":
   lenghtofpno= len(pno)
   try:
      if lenghtofpno!=11:
-         raise ValueError("Phone nunber should be 10 digits!\n============")
+         raise ValueError("Phone nunber should be 10 digits!\n")
      phone_contacts[name]=pno
      
      with open ("Phone-contacts.json","w") as file:
          json.dump(phone_contacts,file)
-         print("Contacts saved successfully !!\n============")  
+         print("Contacts saved successfully !!\n")  
   except ValueError as error:
    print("Error:",error)
-  op=input("You want to again add an contact?\n('Y' for yes and 'Q' for quit)\n\n============")
+  op=input("You want to again add an contact?\n('Y' for yes and 'Q' for quit)\n\n")
   if op in ["q","Q"]:
      break   
 elif option=="2":
          contacts_list=contacts_reader()
          for index,(name,pno) in enumerate(contacts_list.items(),start=1):
-            print(f"\n{index}.{name} ---> {pno}\n============")
+            print(f"\n{index}.{name} ---> {pno}\n")
 elif option=="3":
    contacts_list=contacts_reader()
    while True:
@@ -45,7 +45,7 @@ elif option=="3":
                    print("contact not not found")   
           with open ("Phone-contacts.json","w") as file:
                      json.dump(contacts_list,file)  
-                     print("Task Removed Successfully!\n============") 
+                     print("Task Removed Successfully!\n") 
           break  
               
       
